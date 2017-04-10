d26e3966b019db1254982aca4dd1e39d292b4fe9
Kathryn <kathmo@users.noreply.github.com>
2017-04-09 23:52:50 -0700

    Add effect to key when key is down (audio is playing)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
 function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.code}"]`);
+    const key = document.querySelector(`.key[data-key="${e.code}"]`);
     if (!audio) return;
-    audio.currentTime = 0;
+    audio.currentTime = 0; // resets audio to the beginning
     audio.play();
+    key.classList.add('playing');
 }
 
 window.addEventListener('keydown', playSound);

a25880313cbc70bfd33c4f82f7947943ab29232a
Kathryn <kathmo@users.noreply.github.com>
2017-04-10 12:41:50 -0700

    Remove effect on key when key is up

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,13 @@ function playSound(e) {
     if (!audio) return;
     audio.currentTime = 0; // resets audio to the beginning
     audio.play();
-    key.classList.add('playing');
+    key.classList.add("playing");
 }
 
-window.addEventListener('keydown', playSound);
+function removeEffect(e) {
+    const key = document.querySelector(`.key[data-key="${e.code}"]`);
+    key.classList.remove("playing");
+}
+
+window.addEventListener("keydown", playSound);
+window.addEventListener("keyup", removeEffect);

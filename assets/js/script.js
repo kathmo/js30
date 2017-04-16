728b6bdc0dc9748f44e93994e299e419c893be3c
Kathryn <kathmo@users.noreply.github.com>
2017-04-16 16:40:53 -0700

    Add colors and rearrange sounds

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,11 +5,41 @@ function playSound(e) {
     audio.currentTime = 0; // resets audio to the beginning
     audio.play();
     key.classList.add("playing");
+
+    if (key.classList.contains("hi-hat")) {
+        key.classList.add("playing-hi-hat");
+    }
+
+    if (key.classList.contains("snare")) {
+        key.classList.add("playing-snare");
+    }
+
+    if (key.classList.contains("kick")) {
+        key.classList.add("playing-kick");
+    }
+
+    if (key.classList.contains("ride")) {
+        key.classList.add("playing-ride");
+    }
+
+    if (key.classList.contains("accessory")) {
+        key.classList.add("playing-accessory");
+    }
+
+    if (key.classList.contains("clap")) {
+        key.classList.add("playing-clap");
+    }
 }
 
 function removeEffect(e) {
     const key = document.querySelector(`.key[data-key="${e.code}"]`);
     key.classList.remove("playing");
+    key.classList.remove("playing-hi-hat");
+    key.classList.remove("playing-snare");
+    key.classList.remove("playing-kick");
+    key.classList.remove("playing-ride");
+    key.classList.remove("playing-accessory");
+    key.classList.remove("playing-clap");
 }
 
 window.addEventListener("keydown", playSound);

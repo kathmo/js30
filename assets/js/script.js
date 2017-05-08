7e1352f1c10ba7754b3977bcbb5b5704c06a11ab
Kathryn <kathmo@users.noreply.github.com>
2017-05-07 18:14:31 -0700

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+const panels = document.querySelectorAll('.panel');
+
+function toggleOpen() {
+    this.classList.toggle('open');
+}
+
+function toggleText(e) {
+    if (e.propertyName.includes('flex')) {
+        this.classList.toggle('open-text');
+    }
+}
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleText));

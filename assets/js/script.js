85cb6fba0aaf018a02787d1a1372a4e9ef41a25d
Kathryn <kathmo@users.noreply.github.com>
2017-05-28 00:01:45 -0700

    Add draw function

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ canvas.height = window.innerHeight;
 ctx.strokeStyle = 'blue';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
-ctx.lineWidth = 10;
+ctx.lineWidth = 25;
 
 let isDrawing = false;
 let lastX = 0;
@@ -17,10 +17,20 @@ let lastY = 0;
 function draw(e) {
     if (!isDrawing) return;  // stop drawing if mouse is not clicked down
     console.log(e);
+    ctx.beginPath();
+    // start from
+    ctx.moveTo(lastX, lastY);
+    // go to
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    [lastX, lastY] = [e.offsetX, e.offsetY];
 }
 
 canvas.addEventListener('mousemove', draw);
-canvas.addEventListener('mousedown', () => isDrawing = true);
+canvas.addEventListener('mousedown', (e) => {
+    isDrawing = true;
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+});
 
 canvas.addEventListener('mouseup', () => isDrawing = false);
 canvas.addEventListener('mouseout', () => isDrawing = false);

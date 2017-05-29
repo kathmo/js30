c37ff5290617ca97165a77c45523fd958d289ec1
Kathryn <kathmo@users.noreply.github.com>
2017-05-29 14:52:58 -0700

    Add button to clear canvas

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ To explore JavaScript
 * [```offsetX``` property](https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/offsetX)
 * [array destructuring](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment) to assign variables in a single line
 * [```dataset``` property](https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/dataset)
+* [```clearRect()```](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/clearRect) to clear the canvas
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ const canvas = document.querySelector('#canvas');
 const ctx = canvas.getContext('2d');
 const colorBtns = document.querySelectorAll('.color');
 const sizeBtns = document.querySelectorAll('.size');
+const clearBtn = document.querySelector('#clear');
 
 // make the canvas as large as the window
 canvas.width = window.innerWidth;
@@ -39,12 +40,6 @@ function draw(e) {
     hue++;
 }
 
-canvas.addEventListener('mousemove', draw);
-canvas.addEventListener('mousedown', (e) => {
-    isDrawing = true;
-    [lastX, lastY] = [e.offsetX, e.offsetY];
-});
-
 function setColor(e) {
     color = this.dataset.color;
 }
@@ -53,8 +48,21 @@ function setLineWidth(e) {
     ctx.lineWidth = this.dataset.size;
 }
 
+function clearCanvas(e) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mousedown', (e) => {
+    isDrawing = true;
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+
 canvas.addEventListener('mouseup', () => isDrawing = false);
 canvas.addEventListener('mouseout', () => isDrawing = false);
 
+
+// button events
 colorBtns.forEach(btn => btn.addEventListener('click', setColor));
 sizeBtns.forEach(btn => btn.addEventListener('click', setLineWidth));
+clearBtn.addEventListener('click', clearCanvas);

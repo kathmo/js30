8644913d92032683bea38fa6474864c341337921
Kathryn <kathmo@users.noreply.github.com>
2017-05-29 14:35:04 -0700

    Add buttons to change line width

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 const canvas = document.querySelector('#canvas');
 const ctx = canvas.getContext('2d');
 const colorBtns = document.querySelectorAll('.color');
+const sizeBtns = document.querySelectorAll('.size');
 
 // make the canvas as large as the window
 canvas.width = window.innerWidth;
@@ -14,21 +15,28 @@ ctx.lineWidth = 25;
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
+let color = '';
+let hue = 0;
 
 function draw(e) {
     if (!isDrawing) return;  // stop drawing if mouse is not clicked down
     console.log(e);
+
+    if (color != "rainbow") {
+        ctx.strokeStyle = color;
+    } else {
+        ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+    }
+
     ctx.beginPath();
     // start from
     ctx.moveTo(lastX, lastY);
     // go to
     ctx.lineTo(e.offsetX, e.offsetY);
     ctx.stroke();
-    [lastX, lastY] = [e.offsetX, e.offsetY];
-}
 
-function setColor(e) {
-    ctx.strokeStyle = this.dataset.color;
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+    hue++;
 }
 
 canvas.addEventListener('mousemove', draw);
@@ -37,6 +45,16 @@ canvas.addEventListener('mousedown', (e) => {
     [lastX, lastY] = [e.offsetX, e.offsetY];
 });
 
+function setColor(e) {
+    color = this.dataset.color;
+}
+
+function setLineWidth(e) {
+    ctx.lineWidth = this.dataset.size;
+}
+
 canvas.addEventListener('mouseup', () => isDrawing = false);
 canvas.addEventListener('mouseout', () => isDrawing = false);
+
 colorBtns.forEach(btn => btn.addEventListener('click', setColor));
+sizeBtns.forEach(btn => btn.addEventListener('click', setLineWidth));
